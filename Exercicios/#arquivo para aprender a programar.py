b02491c047685e27a311f80a12cfdcd27001db5f
zaureliu <aajustino@icloud.com>
2025-11-30 20:48:55 -0300

    Create #arquivo para aprender a programar.py

---
+++ Exercicios/#arquivo para aprender a programar.py
@@ -0,0 +1,9 @@
+#arquivo para aprender a programar
+
+nome  = input ('qual é seu nome? ')
+idade = input ('qual sua idade? ')
+peso  = input ('qual seu peso? ')
+ 
+print (nome,',', idade,'anos','e',peso,'kg')
+
+#teste de commit pelo mac
