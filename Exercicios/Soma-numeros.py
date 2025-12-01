e38f84cfabd5a12a67ca93874834c485e1889a28
Anderson A. Justino <aajustino@icloud.com>
2025-11-30 21:43:32 -0300

    Update Soma-numeros.py

--- Exercicios/Soma-numeros.py
+++ Exercicios/Soma-numeros.py
@@ -4,4 +4,7 @@ n1=int(input('Digite um número: '))
 n2=int(input('Digite um segundo número: '))
 s=(n1+n2)
 
-print('A soma dos números é:',s)
+# print('A soma de',n1,'e',n2,'é:',s)
+print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+
+
