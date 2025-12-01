e25d6a9842953f054ae9ea23e49582d02485332b
Anderson A. Justino <aajustino@icloud.com>
2025-11-30 21:15:22 -0300

    Delete #arquivo para aprender a programar.py

--- Exercicios/#arquivo para aprender a programar.py
+++
@@ -1,8 +0,0 @@
-#arquivo para aprender a programar
-
-nome  = input ('qual é seu nome? ')
-idade = input ('qual sua idade? ')
-peso  = input ('qual seu peso? ')
- 
-print (nome,',', idade,'anos','e',peso,'kg')
-
