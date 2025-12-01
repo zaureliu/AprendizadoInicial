031c82be78543f3bb3b4cf64e03621d0f12bcddb
Anderson A. Justino <aajustino@icloud.com>
2025-11-30 23:45:39 -0300

    Create Desafio-1.py

---
+++ Exercicios/Desafio-1.py
@@ -0,0 +1,22 @@
+dado = (input("Digite algo: "))
+
+print('---Análise da informação inserida---')
+print(f'Tipo primitivo: {type(dado)}')
+
+# filtro: Decimal? Numérico? Digito? Alfabético? Alfanumérico?
+# está em maisúcula? Minúscula? Espaço?
+
+print (f'É numérico? {dado.isnumeric()}')
+
+print (f'é decimal? {dado.isdecimal()}')
+print (f'é digito? {dado.isdigit()}')
+
+print (f'É alfanumérico? {dado.isalnum()}')
+print (f'É alfabético? {dado.isalpha()}')
+
+print (f'Está em maiúscula? {dado.isupper()}')
+print (f'Está em minúscula? {dado.islower()}')
+
+print (f'É somente espaços? {dado.isspace()}')
+print (f'É simbolo? {dado.isascii()}') 
+
