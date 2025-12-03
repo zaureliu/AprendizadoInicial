fb1aa31af88f026ac9208500b9d86c60398ca765
Anderson A. Justino <aajustino@icloud.com>
2025-12-03 01:45:38 -0300

    Update Operacoes-aritmeticas.py

--- Exercicios/Operacoes-aritmeticas.py
+++ Exercicios/Operacoes-aritmeticas.py
@@ -1,1 +1,17 @@
-#Exercicios para treinas operações matemáticas
+#Exercicios para treinar operações matemáticas
+#Ordem de precedência:
+# () parenteses
+# ** exponenciação
+# *, / , //, %
+# + e -
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('digite um segundo valor: '))
+soma  = n1 + n2
+mult  = n1 * n2
+div   = n1 / n2
+divint= n1 // n2
+exp1   = n1 ** n2
+
+print('A soma é {}, a multiplicação é: {}, a divisão é: {}'.format(soma,mult,div))
+print('A divisão inteira é: {}, a divisão em decimal é: {}, raiz quadrada é:{}'.format(divint, div, exp1))
