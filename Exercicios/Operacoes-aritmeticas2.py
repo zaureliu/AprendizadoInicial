3e53316e220ca9c2d720de5afd5dc1e057675c5d
Anderson A. Justino <aajustino@icloud.com>
2025-12-03 23:02:09 -0300

    Update Operacoes-aritmeticas2.py

--- Exercicios/Operacoes-aritmeticas2.py
+++ Exercicios/Operacoes-aritmeticas2.py
@@ -2,4 +2,8 @@
 #Faça um exercicio para mostrar um numero e mostrar seu antecessor e sucessor.abs
 
 numero=int(input('Digite um número: '))
+soma=(numero+1)
+subt=(numero-1)
 
+
+print('o número digitado tem como antecessor {} e seu sucessor é {}.'.format(subt,soma))
