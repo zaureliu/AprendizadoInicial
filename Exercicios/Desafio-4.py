b8c0719bbd37db434ecbf79a6aff9bca0c3e2787
Anderson A. Justino <aajustino@icloud.com>
2025-12-04 04:03:52 -0300

    Update Desafio-4.py

--- Exercicios/Desafio-4.py
+++ Exercicios/Desafio-4.py
@@ -1,3 +1,12 @@
 #Crie um algoritmo para pegar um valor em metros e converta para centimetros e milimetros.abs
 
-dado
+dado=(input('Digite o valor em metros(M): '))
+dado=dado.replace(',','.')
+dado = float(dado)
+
+cm=(dado * 100)
+mm=(dado * 1000)
+
+print('A medida',dado, '''convertido para cm fica: {}cm e convertido para mm fica: {}mm''' .format(cm,mm))
+
+#finalizado
