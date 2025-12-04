0be60eff6956f857730a392484ff5d967471723a
Anderson A. Justino <aajustino@icloud.com>
2025-12-03 23:02:10 -0300

    Create Desafio-3.py

---
+++ Exercicios/Desafio-3.py
@@ -0,0 +1,9 @@
+#Criar um algoritmo que pegue duas notas de um aluno e calcule sua média.abs
+
+aluno=input('Nome do aluno: ')
+nota1=int(input('digite a primeira nota: '))
+nota2=int(input('digite a segunda nota: '))
+
+media=((nota1 + nota2)/2)
+
+print('A média do aluno(a)', aluno, 'é:', media )
