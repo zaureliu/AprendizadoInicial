ff144e34ef633134f44236c5717ab53fe4a5b3b0
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 04:21:24 -0300

    Update Desafio-9.py

--- Exercicios/Desafio-9.py
+++ Exercicios/Desafio-9.py
@@ -1,2 +1,9 @@
 #Solicitar ao usuário o salário e informar o mesmo com 15% de aumento.abs
 
+nome=input('Qual seu nome? ')
+salario_str=input(f'Bem vindo, {nome}! digite o seu salário mensal: ')
+salario_str=salario_str.replace(',','.')
+salario=salario_str(float)
+
+
+print(f'Olá {nome}, seu salário autual é: {salario}.')
