b9a9942f1f1540661857d3b3c40ebfe1f35fb1e1
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 02:31:55 -0300

    Update Desafio-2.py

--- Exercicios/Desafio-2.py
+++ Exercicios/Desafio-2.py
@@ -1,6 +1,8 @@
 #Criar um algoritmo que peça um número ao usuário e mostre na tela o seu dobro
 #triplo e raiz quadrada
 
+
+
 numero=int(input('Digite um numero: '))
 dobro=(numero * 2)
 triplo=(numero * 3)
@@ -8,5 +10,3 @@ raiz=(numero ** 2)
 
 print('O dobro do numero é {}, o seu triplo é: {} e sua raiz quadarada é: {}. '.format(dobro,triplo,raiz))
 
-
-#finalizado
