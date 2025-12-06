bbeef2789289690c00c69e4c3250530190ec03d7
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 02:49:01 -0300

    Update Desafio-2.py

--- Exercicios/Desafio-2.py
+++ Exercicios/Desafio-2.py
@@ -3,9 +3,9 @@
 
 
 numero=int(input('Digite um numero: '))     #Solicita ao usuário que digite um número, nesse caso apenas nº inteiro.
-dobro=(numero * 2)      #Operação de cálculo de multiplicação.
-triplo=(numero * 3)     #Operação de cálculo de multiplicação.
-raiz=(numero ** 2)      #Operação de cálculo de exponenciação.
+dobro=(numero * 2)      #Atribuição de variável e operação de cálculo de multiplicação.
+triplo=(numero * 3)     #Atribuição de variáve e operação de cálculo de multiplicação.
+raiz=(numero ** 2)      #Atribuição de variável e operação de cálculo de exponenciação.
 
 print('O dobro do numero é {}, o seu triplo é: {} e sua raiz quadarada é: {}. '.format(dobro,triplo,raiz))      #Mostra na tela o resultado.
-                                                                                     #format insere as informações dentro dos colchetes, conforme a ordem que eu inserir.
+                                                                               #format pega e insere as informações das variáveis nos colchetes, conforme ordem que colocar nos parênteses.
