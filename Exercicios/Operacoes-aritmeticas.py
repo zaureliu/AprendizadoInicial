44398cd6d82bf375a4b2d0b71e8a3b474f9e81c1
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 03:11:41 -0300

    Update Operacoes-aritmeticas.py

--- Exercicios/Operacoes-aritmeticas.py
+++ Exercicios/Operacoes-aritmeticas.py
@@ -1,19 +1,20 @@
-#Exercicios para treinar operações matemáticas
+#Exercicios para treinar operações matemáticas.
 #Ordem de precedência:
 # () parenteses
-# ** e  xponenciação
+# ** exponenciação
 # *, / , //, %
 # + e -
 
 
-
 n1 = int(input('Digite um valor: '))
 n2 = int(input('digite um segundo valor: '))
-soma  = n1 + n2
-mult  = n1 * n2
-div   = n1 / n2
-divint= n1 // n2
-exp1   = n1 ** n2
+soma  = n1 + n2     #Soma.
+mult  = n1 * n2     #Multiplicação.     
+div   = n1 / n2     #Divisão com resultado em decimal.
+divint= n1 // n2    #Divisão com resultado inteiro.
+exp1   = n1 ** n2   #Operação de exponenciação.
 
 print('A soma é {}, a multiplicação é: {}, a divisão é: {}'.format(soma,mult,div))
 print('A divisão inteira é: {}, a divisão em decimal é: {}, raiz quadrada é:{}'.format(divint, div, exp1))
+
+#função format nas duas ultimas linhas pegam o resultado das variáveis e colocam nos colchetes apenas.
