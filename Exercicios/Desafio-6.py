e75a8b1c85e3905ca1140f1e11c5fa77aa2c5998
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 02:31:32 -0300

    Update Desafio-6.py

--- Exercicios/Desafio-6.py
+++ Exercicios/Desafio-6.py
@@ -1,8 +1,10 @@
 #Crie um algoritmo que leia a quantia em reais e converta em dólar.abs
 
+
+
 carteira=(input('Digite o valor que você tem na carteira: '))
-valor=float(carteira)               #float é para número decimal
-dolar=(5.31)                        #aqui nao vai float, já defini valor fixo para a var.
+valor=float(carteira)             #float é para converter em número decimal.
+dolar=(5.31)                      #aqui nao vai float, já defini valor fixo para a var.
 
 conv=valor / dolar
 print('O saldo convertido em dólar atualmente é {:.2f} dólares.'.format(conv))
