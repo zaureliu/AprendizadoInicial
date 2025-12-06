6163867608834d9c72c6ab59060386e4a9913ee0
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 03:15:32 -0300

    Update Soma-numeros.py

--- Exercicios/Soma-numeros.py
+++ Exercicios/Soma-numeros.py
@@ -2,11 +2,10 @@
 
 
 
-n1=int(input('Digite um número: '))
-n2=int(input('Digite um segundo número: '))
+n1=int(input('Digite um número: '))     #Solicita número INTeiro.
+n2=int(input('Digite um segundo número: '))     #Solicita outro número INTeiro.
 s=(n1+n2)
 
-# print('A soma de',n1,'e',n2,'é:',s) (formato antigo)
+
 print('A soma entre {} e {} vale {}'.format(n1,n2,s))
-
-
+#format vai pegar os dados das variáveis e inserir nos colchetes. 
