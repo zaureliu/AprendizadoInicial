3e191fdb0b5fb114e5d1426598baf16b12cb9044
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 00:16:55 -0300

    Update Desafio-7.py

--- Exercicios/Desafio-7.py
+++ Exercicios/Desafio-7.py
@@ -1,3 +1,18 @@
 #Ler largura e altura de uma parede, e calcular sua area em m², em seguida a quantidade de tinta
 # cada L de tinta pinta 2m²
 
+altura  = input("Qual a altura da parede? ")
+largura = input("Qual a largura da parede? ")
+altura = altura.replace(',','.')
+largura = largura.replace(',','.')
+largura = float(largura)
+altura = float(altura)
+
+
+
+area =(altura * largura)
+
+totaltinta=(area / 2)
+
+print('A parede mede {}m² e a quantidade de tinta necessária é {} Litros'.format(area,totaltinta))
+
