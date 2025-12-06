ffa30e443ed6edd4dc97b04e9a5ca56563889717
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 03:13:59 -0300

    Update Operacoes-aritmeticas2.py

--- Exercicios/Operacoes-aritmeticas2.py
+++ Exercicios/Operacoes-aritmeticas2.py
@@ -1,6 +1,4 @@
-#Exercicios para treinar operações matemáticas
 #Faça um exercicio para mostrar um numero e mostrar seu antecessor e sucessor.abs
-
 
 
 numero=int(input('Digite um número: '))   #Solicita informação (apenas numero INTeiro).
