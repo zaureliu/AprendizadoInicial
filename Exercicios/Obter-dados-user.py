c10926a4764385402a71dba2426066e6a29ead25
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 03:07:34 -0300

    Update Obter-dados-user.py

--- Exercicios/Obter-dados-user.py
+++ Exercicios/Obter-dados-user.py
@@ -1,4 +1,6 @@
 # Exercicio para ler o nome da pessoa e imprimir mensagem de boas vindas.
 
-nome = input("digite seu nome:")
-print('Prazer em te conhecer,', nome,"!!")
+
+
+nome = input("digite seu nome:")    #Pede informação ao usuário
+print('Prazer em te conhecer,', nome,"!!")  #Imprime o resultado com informação adicional.
