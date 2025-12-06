d5bb18b7c28da5d1830ec250d3d0bd2061be679a
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 02:21:37 -0300

    Update Desafio-8.py

--- Exercicios/Desafio-8.py
+++ Exercicios/Desafio-8.py
@@ -1,11 +1,11 @@
 #Ler o preço de um produto e mostrá-lo com desconto de 5%.abs
 
-produto=input('Qual o produto? ')          #solicita o nome do produto
-valor_str=input(f'Qual o valor de {produto}? ')     #solicita o valor do produto(ele mostra qual produto escolhi.)
+produto=input('Qual o produto? ')          #Solicita o nome do produto
+valor_str=input(f'Qual o valor de {produto}? ')     #Solicita o valor do produto(ele mostra qual produto escolhi.)
 valor_str=valor_str.replace(',','.')    #Fazer a conversão caso o usuário digitar vírgula e entender como número.
-valor=float(valor_str)          #converter a informação digitada em valor decimal.
+valor=float(valor_str)          #Converter a informação digitada em valor decimal.
 
-desconto=(valor * (5/100))      #operação para achar o valor de desconto
-desconto1=(valor - desconto)    #operação para mostrar o valor com desconto.
+desconto=(valor * (5/100))      #Operação para achar o valor de desconto.
+desconto1=(valor - desconto)    #Operação para mostrar o valor com desconto.
 
 print('o valor do(a) {} está custando {} R$ com 5%'' de desconto.'.format(produto,desconto1))        #Mostrar para o usuário o produto escolhido com o valor com 5% de desconto.
