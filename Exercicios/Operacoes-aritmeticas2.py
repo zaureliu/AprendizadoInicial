82b0f1750459fee732dad71d74cff34b9b8634cf
Anderson A. Justino <aajustino@icloud.com>
2025-12-06 02:30:44 -0300

    Update Operacoes-aritmeticas2.py

--- Exercicios/Operacoes-aritmeticas2.py
+++ Exercicios/Operacoes-aritmeticas2.py
@@ -1,6 +1,8 @@
 #Exercicios para treinar operações matemáticas
 #Faça um exercicio para mostrar um numero e mostrar seu antecessor e sucessor.abs
 
+
+
 numero=int(input('Digite um número: '))
 soma=(numero+1)
 subt=(numero-1)
