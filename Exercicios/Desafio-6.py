8af200f6aa1efc08f324d632cc0a82f2c4ee8227
Anderson A. Justino <aajustino@icloud.com>
2025-12-07 04:24:55 -0300

    Update Desafio-6.py

--- Exercicios/Desafio-6.py
+++ Exercicios/Desafio-6.py
@@ -2,11 +2,9 @@
 
 
 
-carteira=(input('Digite o valor que você tem na carteira: '))
-valor=float(carteira)             #float é para converter em número decimal.
-dolar=(5.31)                      #aqui nao vai float, já defini valor fixo para a var.
-
-conv=valor / dolar
+carteira=float(input('Digite o valor que você tem na carteira: '))
+dolar=(5.31)                #valor fixo p/ variavel
+conv=carteira / dolar
 print('O saldo convertido em dólar atualmente é {:.2f} dólares.'.format(conv))
  #                                                 ^
-                                 #comando para delimitar a 2 casas depois da virgula.
+                        #comando para delimitar a 2 casas depois da virgula.
