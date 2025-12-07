48830957fd7c988efabb396b06338ad44f76ceff
Anderson A. Justino <aajustino@icloud.com>
2025-12-07 04:08:37 -0300

    Update Desafio-5.py

--- Exercicios/Desafio-5.py
+++ Exercicios/Desafio-5.py
@@ -1,10 +1,18 @@
 #Pedir um numero de 1 a 9 e formar a tabuada inteira do mesmo.
 
 
-numero = int(input('Qual tabuada você quer saber? '))   #Atribui uma variável e insere um valor INTeiro.
-print(f'tabuada do número:{numero}')       #Mostrar qual tabuada o usuário escolheu.
+numero = int(input('Qual tabuada você quer saber? '))   
+#Atribui uma variável e insere um valor INTeiro.
+
+print(f'tabuada do número:{numero}')       
+#Mostrar qual tabuada o usuário escolheu.
 
 
-for i in range(1,11):      #Função para repetir numa quantidade especifica uma determinada operação.
-    total = numero * i     #Conforme o range escolhido, ele vai substituindo 'i' pelo numero em ordem crescente de 1 até 10.
-    print(f'{numero} x {i}= {total}')      #Mostra a informação(o range se aplica também aqui, mostrando de 1 até 10.)
+for i in range(1,11):      
+    #Função para repetir numa quantidade especifica uma determinada operação.
+    
+    total = numero * i     
+    #Conforme o range escolhido, ele troca 'i' pela em ordem crescente de 1 até 10.
+
+    print(f'{numero} x {i}= {total}')      
+    #Mostra a informação(o range se aplica também aqui, mostrando de 1 até 10.)
