16092f7b615610ea328c148d8baebbb41684f927
Anderson A. Justino <aajustino@icloud.com>
2025-12-07 04:08:34 -0300

    Update Desafio-4.py

--- Exercicios/Desafio-4.py
+++ Exercicios/Desafio-4.py
@@ -1,12 +1,18 @@
 #Crie um algoritmo para pegar valor em metros e converta para centímetros e milímetros.
 
 
-dado=(input('Digite o valor em metros(M): '))       #Solicita info ao usuário.
-dado=dado.replace(',','.')      #Substitui virgula pelo ponto na str inserida.
-dado = float(dado)      #Converte a str em número decimal.
+dado=(input('Digite o valor em metros(M): '))      
+#Solicita info ao usuário.
+
+dado=dado.replace(',','.')      
+#Substitui virgula pelo ponto na str inserida.
+
+dado = float(dado)      
+#Converte a str em número decimal.
 
 cm=(dado * 100)     #Cálculo de conversão.
 mm=(dado * 1000)    #Cálculo de conversão.
 
-print('A medida',dado, '''convertido para cm fica: {}cm e convertido para mm fica: {}mm''' .format(cm,mm)) #Insere na tela as informações já calculadas.
-                                                                                                #format apenas pega as informações das variavéis e joga nos colchetes.
+print('A medida',dado,'m' ''' convertido para cm fica: {}cm e convertido para mm fica: {}mm''' .format(cm,mm)) 
+#Insere na tela as informações já calculadas.
+#format apenas pega as informações das variavéis e joga nos colchetes.
