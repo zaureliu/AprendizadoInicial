1dd268b422e56be4a8d7f1b580d1218b3bf96ae4
Anderson A. Justino <aajustino@icloud.com>
2025-12-07 04:13:50 -0300

    Update Desafio-5.py

--- Exercicios/Desafio-5.py
+++ Exercicios/Desafio-5.py
@@ -10,9 +10,9 @@ print(f'tabuada do número:{numero}')
 
 for i in range(1,11):      
     #Função para repetir numa quantidade especifica uma determinada operação.
-    
+
     total = numero * i     
     #Conforme o range escolhido, ele troca 'i' pela em ordem crescente de 1 até 10.
 
-    print(f'{numero} x {i}= {total}')      
+    print(f'{numero} x {i:2} = {total}')      
     #Mostra a informação(o range se aplica também aqui, mostrando de 1 até 10.)
