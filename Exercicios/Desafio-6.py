684d6a3d17c21a566bf4f4e9122c3753dbee898d
zaureliu <aajustino@icloud.com>
2025-12-19 12:37:10 -0300

    Update Desafio-6.py

--- Exercicios/Desafio-6.py
+++ Exercicios/Desafio-6.py
@@ -5,6 +5,6 @@
 carteira=float(input('Digite o valor que você tem na carteira: '))
 dolar=(5.31)                #valor fixo p/ variavel
 conv=carteira / dolar
-print('O saldo convertido em dólar atualmente é {:.2f} dólares.'.format(conv))
- #                                                 ^
+print('O saldo convertido em dólar atualmente é {:.2f} dólares.'.format(conv))      
+
                         #comando para delimitar a 2 casas depois da virgula.
