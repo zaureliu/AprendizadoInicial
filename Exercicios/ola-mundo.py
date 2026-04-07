33a3165c3ee1bada0a70fcd98784fd58d0437b2a
Anderson A. Justino <aajustino@icloud.com>
2026-04-07 10:12:25 -0300

    Update ola-mundo.py

--- Exercicios/ola-mundo.py
+++ Exercicios/ola-mundo.py
@@ -1,5 +1,16 @@
-#Tradição clássica no mundo da progração!
+def funcao():
+    variavel_local = 10
+    print(variavel_local)  # Acessível dentro da função
 
 
-print('olá mundo!')
- 
+variavel_global = 20
+
+
+def funcao2():
+    print(variavel_global)  # Acessível de qualquer lugar
+
+
+funcao()  # Imprime 10
+funcao2()  # Imprime 20
+print(variavel_global)  # Imprime 20
+print(variavel_local)  # Gera um erro, a variável não está definida neste escopo.
