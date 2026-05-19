256891fe84d7ff064bb7eeb0b74e38dc6f513e43
Anderson A. Justino <aajustino@icloud.com>
2026-05-19 12:05:49 -0300

    Update Obter-dados-user.py

--- Exercicios/Obter-dados-user.py
+++ Exercicios/Obter-dados-user.py
@@ -2,5 +2,6 @@
 
 
 
-nome = input("digite seu nome:")    #Pede informação ao usuário
-print('Prazer em te conhecer,', nome,"!!")  #Imprime o resultado com informação adicional.
+nome = input("digite seu nome: ")    #Pede informação ao usuário
+print('Prazer em te conhecer,{:^20}'.format(nome),"!!")  #Imprime o resultado com informação adicional.
+    
